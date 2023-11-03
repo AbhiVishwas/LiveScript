c917a88955b8a7172f3dd80d32b7ec33be35249c
Abhi Vishwas <34084368+AbhiVishwas@users.noreply.github.com>
2023-11-03 00:28:06 -0700

    Update script.js
    switched onresult and looped through finalTranscript

--- script.js
+++ script.js
@@ -1,43 +1,61 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const transcript = document.querySelector("#transcript");
-    const startButton = document.querySelector("#start");
-    const stopButton = document.querySelector("#stop");
-    let recognition;
-    let finalTranscript = "";
+  const transcript = document.querySelector("#transcript");
+  const startButton = document.querySelector("#start");
+  const stopButton = document.querySelector("#stop");
+  let recognition;
+  let finalTranscript = "";
 
-    if ("webkitSpeechRecognition" in window) {
-        recognition = new webkitSpeechRecognition();
-        recognition.continuous = true;
-        recognition.interimResults = true;
+  if ("webkitSpeechRecognition" in window) {
+    recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = false;
 
-        recognition.onstart = () => {
-            transcript.innerHTML = "Listening: ";
-            finalTranscript = "";
-        };
+    recognition.onstart = () => {
+      transcript.innerHTML = "Listening: ";
+      finalTranscript = "";
+    };
 
-        recognition.onend = () => {
-            transcript.innerHTML += finalTranscript + " (Not listening)";
-        };
+    recognition.onend = () => {
+      transcript.innerHTML += finalTranscript + " (Not listening)";
+    };
 
-        startButton.addEventListener("click", () => {
-            transcript.innerHTML = "Listening: ";
-            finalTranscript = "";
-            recognition.start();
-        });
+    startButton.addEventListener("click", () => {
+      transcript.innerHTML = "Listening: ";
+      finalTranscript = "";
+      recognition.start();
+    });
 
-        stopButton.addEventListener("click", () => {
-            recognition.stop();
-        });
+    stopButton.addEventListener("click", () => {
+      recognition.stop();
+    });
 
-        recognition.onresult = (event) => {
-            const result = event.results[event.results.length - 1];
-            const text = result[0].transcript.trim();
+    recognition.onresult = (event) => {
+      const result = event.results[0];
+      const text = result[0].transcript.trim();
 
-            if (text) {
-                finalTranscript = Array.from(new Set(finalTranscript.split(" "))).join(" ") + " " + text;
-            }
-        };
-    } else {
-        transcript.innerHTML = "Speech Recognition Not Available";
+      if (!finalTranscript.includes(text)) {
+        finalTranscript += text + " ";
+        transcript.innerHTML = "Listening: " + finalTranscript;
+      }
+    };
+  } else {
+    transcript.innerHTML = "Speech Recognition Not Available";
+  }
+
+  function getUniqueWords(text) {
+    const currentWords = text.split(" ");
+    const previousWords = finalTranscript.split(" ");
+    const uniqueWords = [];
+
+    for (const word of currentWords) {
+      if (
+        !previousWords.includes(word) &&
+        !uniqueWords.includes(word)
+      ) {
+        uniqueWords.push(word);
+      }
     }
+
+    return uniqueWords;
+  }
 });
