c4e264bdcde4de90a7546a68fd94334ae916544a
Abhi Vishwas <34084368+AbhiVishwas@users.noreply.github.com>
2023-11-03 00:38:02 -0700

    Update script.js
    trigger test

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ document.addEventListener("DOMContentLoaded", function () {
   const transcript = document.querySelector("#transcript");
   const startButton = document.querySelector("#start");
   const stopButton = document.querySelector("#stop");
+  const triggerInput = document.querySelector("#trigger-input");
+  const addTriggerButton = document.querySelector("#add-trigger");
+  const triggerList = document.querySelector("#trigger-list");
   let recognition;
   let finalTranscript = "";
+
+  const triggerWords = [];
 
   if ("webkitSpeechRecognition" in window) {
     recognition = new webkitSpeechRecognition();
@@ -29,9 +34,25 @@ document.addEventListener("DOMContentLoaded", function () {
       recognition.stop();
     });
 
+    addTriggerButton.addEventListener("click", () => {
+      const triggerWord = triggerInput.value.trim().toLowerCase();
+      if (triggerWord !== "" && !triggerWords.includes(triggerWord)) {
+        triggerWords.push(triggerWord);
+        triggerInput.value = "";
+        displayTriggerWord(triggerWord);
+      }
+    });
+
     recognition.onresult = (event) => {
       const result = event.results[0];
       const text = result[0].transcript.trim();
+      const words = text.split(" ");
+
+      for (const word of words) {
+        if (triggerWords.includes(word.toLowerCase())) {
+          highlightWord(word);
+        }
+      }
 
       if (!finalTranscript.includes(text)) {
         finalTranscript += text + " ";
@@ -42,20 +63,17 @@ document.addEventListener("DOMContentLoaded", function () {
     transcript.innerHTML = "Speech Recognition Not Available";
   }
 
-  function getUniqueWords(text) {
-    const currentWords = text.split(" ");
-    const previousWords = finalTranscript.split(" ");
-    const uniqueWords = [];
+  function displayTriggerWord(word) {
+    const li = document.createElement("li");
+    li.innerText = word;
+    triggerList.appendChild(li);
+  }
 
-    for (const word of currentWords) {
-      if (
-        !previousWords.includes(word) &&
-        !uniqueWords.includes(word)
-      ) {
-        uniqueWords.push(word);
-      }
-    }
-
-    return uniqueWords;
+  function highlightWord(word) {
+    const regex = new RegExp(`(\\b${word}\\b)`, "gi");
+    transcript.innerHTML = transcript.innerHTML.replace(
+      regex,
+      '<span style="background-color: yellow">$1</span>'
+    );
   }
 });
